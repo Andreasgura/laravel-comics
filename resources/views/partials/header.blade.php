3bca7339004e52e2f6dc900a6b51db73454cb807
Andrea Sgura <andre-56@hotmail.it>
2024-05-20 15:46:57 +0200

    db.php and header pt1

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,15 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header>
+        <div class="container  ">
+            <div class="d-flex justify-content-between">
+                <div class=" py-3"><img src="/img/dc-logo.png" alt="logo"></div>
+                <div class="d-flex align-items-center">
+                    <ul class="text-uppercase d-flex h-100 mb-0 ">
+                        <li class="d-flex align-items-center" v-for="(menu, index) in menuList" :key="index">
+                            <a :href="menu.path">{{ menu.name }}</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </header>
