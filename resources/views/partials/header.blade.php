76f0c8b5a68803bbff1e40e42093189b3bba3c51
Andrea Sgura <andre-56@hotmail.it>
2024-05-20 16:09:51 +0200

    header menuList from config.db

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,8 +4,10 @@
                 <div class=" py-3"><img src="/img/dc-logo.png" alt="logo"></div>
                 <div class="d-flex align-items-center">
                     <ul class="text-uppercase d-flex h-100 mb-0 ">
-                        <li class="d-flex align-items-center" v-for="(menu, index) in menuList" :key="index">
-                            <a :href="menu.path">{{ menu.name }}</a>
+                        <li>
+                            @foreach ($menuList as $menu)
+                                <a href="{{ $menu['path'] }}">{{ $menu['name'] }}</a>
+                            @endforeach
                         </li>
                     </ul>
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $menuList = config('db.menuList');
+    return view('home',compact('menuList'));
 });
